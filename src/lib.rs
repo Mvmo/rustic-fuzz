3ceceb91dd460ca599640c8a50218b72de664c5d
Mvmo <mvmogo@gmail.com>
2023-08-17 06:16:12 +0200

    Implement in place fn and added test cases

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,10 @@ fn fuzzy_sort(to_sort: Vec<String>, input: &str) -> Vec<String> {
     clone
 }
 
+fn fuzzy_sort_in_place(to_sort: &mut Vec<String>, input: &str) {
+    to_sort.sort_by_key(|s| levenshtein_distance(s, input));
+}
+
 fn levenshtein_distance(u: &str, v: &str) -> u32 {
     let u_chars: Vec<char> = u.chars().collect();
     let v_chars: Vec<char> = v.chars().collect();
@@ -37,11 +41,28 @@ fn levenshtein_distance(u: &str, v: &str) -> u32 {
 
 #[test]
 fn fuzzy_sort_test() {
-    let arr = vec!["xxx".to_string(), "yyy".to_string(), "xx".to_string()];
-    let str = "xxx";
+    let test_cases = vec![
+        (
+            vec!["xxx".to_string(), "yyy".to_string(), "xx".to_string()],
+            "xxx",
+            vec!["xxx".to_string(), "xx".to_string(), "yyy".to_string()]
+        ),
+        (
+            vec!["apple".to_string(), "banana".to_string(), "cherry".to_string()],
+            "banana",
+            vec!["banana".to_string(), "apple".to_string(), "cherry".to_string()]
+        ),
+        (
+            vec!["rust".to_string(), "is".to_string(), "awesome".to_string()],
+            "awesome",
+            vec!["awesome".to_string(), "rust".to_string(), "is".to_string()]
+        ),
+    ];
 
-    let sorted = fuzzy_sort(arr, str);
-    assert_eq!(vec!["xxx", "xx", "yyy"], sorted);
+    test_cases.iter().for_each(|(arr, input, expected_sorted)| {
+        let sorted = fuzzy_sort(arr.clone(), input);
+        assert_eq!(sorted, *expected_sorted);
+    });
 }
 
 #[test]
@@ -50,7 +71,8 @@ fn levenshtein_distance_test() {
         ("hello, world", "hello- world", 1),
         ("murice", "maurice", 1),
         ("delete", "insert", 5),
-        ("Tier", "Tor", 2)
+        ("Tier", "Tor", 2),
+        ("banana", "cherry", 6)
     ].iter()
     .for_each(|(u, v, expected_distance)| {
         let distance = levenshtein_distance(u, v);
